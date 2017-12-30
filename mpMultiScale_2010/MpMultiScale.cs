f28465e700be5f5320ce27d6bef342c4e3d306ce
Modis <cjmodis@gmail.com>
2017-12-30 23:58:36 +0300

    Localization

--- mpMultiScale_2010/MpMultiScale.cs
+++ mpMultiScale_2010/MpMultiScale.cs
@@ -15,6 +15,8 @@ namespace mpMultiScale
 {
     public class MpMultiScale
     {
+        private const string LangItem = "mpMultiScale";
+
         [CommandMethod("ModPlus", "mpMultiScale", CommandFlags.UsePickSet)]
         public void Main()
         {
@@ -32,23 +34,23 @@ namespace mpMultiScale
                 using (tr)
                 {
                     var opts = new PromptSelectionOptions();
-                    opts.Keywords.Add("Копия");
+                    opts.Keywords.Add(Language.GetItem(LangItem, "msg1"));
                     var kws = opts.Keywords.GetDisplayString(true);
-                    opts.MessageForAdding = "\n" + "Выберите объекты: " + kws;
+                    opts.MessageForAdding = "\n" + Language.GetItem(LangItem, "msg2") + kws;
                     // Implement a callback for when keywords are entered
-                    opts.KeywordInput += delegate(object sender, SelectionTextInputEventArgs e)
+                    opts.KeywordInput += delegate (object sender, SelectionTextInputEventArgs e)
                     {
-                        if (e.Input.Equals("Копия"))
+                        if (e.Input.Equals(Language.GetItem(LangItem, "msg1")))
                             isCopy = !isCopy;
                     };
                     var res = ed.GetSelection(opts);
                     if (res.Status != PromptStatus.OK) return;
                     var selSet = res.Value;
-                    var _idArray = selSet.GetObjectIds();
-                    if (_idArray == null) return;
-                    var idArray = isCopy ? SetCopy(_idArray) : _idArray;
+                    var idArr = selSet.GetObjectIds();
+                    if (idArr == null) return;
+                    var idArray = isCopy ? SetCopy(idArr) : idArr;
 
-                    var doubleOpt = new PromptDoubleOptions("\n" + "Масштаб: ")
+                    var doubleOpt = new PromptDoubleOptions("\n" + Language.GetItem(LangItem, "msg3"))
                     {
                         AllowNegative = false,
                         AllowNone = false,
@@ -58,41 +60,63 @@ namespace mpMultiScale
                     if (doubleRes.Status != PromptStatus.OK) return;
 
                     var pdOpt = new PromptKeywordOptions(string.Empty);
-                    var sVal = "Центр"; // Начальное значение
+                    var sVal = Language.GetItem(LangItem, "kw1"); // Начальное значение
                     pdOpt.AllowArbitraryInput = true;
                     pdOpt.AllowNone = true;
+                    //pdOpt.SetMessageAndKeywords(
+                    //    "\n" + "Выберите базовую точку: " + "<" + sVal +
+                    //    ">: " + "[Центр/ЛНиз/ЛВерх/ПНиз/ПВерх/СНиз/СВерх/СЛево/СПраво]",
+                    //    "Центр ЛНиз ЛВерх ПНиз ПВерх СНиз СВерх СЛево СПраво");
+
                     pdOpt.SetMessageAndKeywords(
-                        "\n" + "Выберите базовую точку: " + "<" + sVal +
-                        ">: " + "[Центр/ЛНиз/ЛВерх/ПНиз/ПВерх/СНиз/СВерх/СЛево/СПраво]",
-                        "Центр ЛНиз ЛВерх ПНиз ПВерх СНиз СВерх СЛево СПраво");
+                        "\n" + Language.GetItem(LangItem, "msg4") + "<" + sVal +
+                        ">: " + "[" +
+                        Language.GetItem(LangItem, "kw1") + "/" +
+                        Language.GetItem(LangItem, "kw2") + "/" +
+                        Language.GetItem(LangItem, "kw3") + "/" +
+                        Language.GetItem(LangItem, "kw5") + "/" +
+                        Language.GetItem(LangItem, "kw4") + "/" +
+                        Language.GetItem(LangItem, "kw6") + "/" +
+                        Language.GetItem(LangItem, "kw7") + "/" +
+                        Language.GetItem(LangItem, "kw8") + "/" +
+                        Language.GetItem(LangItem, "kw9") + "]",
+                        Language.GetItem(LangItem, "kw1") + " " +
+                        Language.GetItem(LangItem, "kw2") + " " +
+                        Language.GetItem(LangItem, "kw3") + " " +
+                        Language.GetItem(LangItem, "kw5") + " " +
+                        Language.GetItem(LangItem, "kw4") + " " +
+                        Language.GetItem(LangItem, "kw6") + " " +
+                        Language.GetItem(LangItem, "kw7") + " " +
+                        Language.GetItem(LangItem, "kw8") + " " +
+                        Language.GetItem(LangItem, "kw9"));
                     var promptres = ed.GetKeywords(pdOpt);
                     if (promptres.Status != PromptStatus.OK) return;
                     sVal = promptres.StringResult;
                     foreach (var objId in idArray)
                     {
-                        var ent = (Entity) tr.GetObject(objId, OpenMode.ForWrite);
+                        var ent = (Entity)tr.GetObject(objId, OpenMode.ForWrite);
                         var extPts = ent.GeometricExtents;
                         var pt1 = extPts.MinPoint;
                         var pt3 = extPts.MaxPoint;
                         var pt = new Point3d();
-                        if (sVal.Equals("Центр"))
-                            pt = new Point3d((pt1.X + pt3.X)/2, (pt1.Y + pt3.Y)/2, (pt1.Z + pt3.Z)/2);
-                        else if (sVal.Equals("ЛНиз"))
+                        if (sVal.Equals(Language.GetItem(LangItem, "kw1")))
+                            pt = new Point3d((pt1.X + pt3.X) / 2, (pt1.Y + pt3.Y) / 2, (pt1.Z + pt3.Z) / 2);
+                        else if (sVal.Equals(Language.GetItem(LangItem, "kw2")))
                             pt = pt1;
-                        else if (sVal.Equals("ЛВерх"))
+                        else if (sVal.Equals(Language.GetItem(LangItem, "kw3")))
                             pt = new Point3d(pt1.X, pt3.Y, 0.0);
-                        else if (sVal.Equals("ПВерх"))
+                        else if (sVal.Equals(Language.GetItem(LangItem, "kw4")))
                             pt = pt3;
-                        else if (sVal.Equals("ПНиз"))
+                        else if (sVal.Equals(Language.GetItem(LangItem, "kw5")))
                             pt = new Point3d(pt3.X, pt1.Y, 0.0);
-                        else if (sVal.Equals("СНиз"))
-                            pt = new Point3d((pt1.X + pt3.X)/2, pt1.Y, 0.0);
-                        else if (sVal.Equals("СВерх"))
-                            pt = new Point3d((pt1.X + pt3.X)/2, pt3.Y, 0.0);
-                        else if (sVal.Equals("СЛево"))
-                            pt = new Point3d(pt1.X, (pt1.Y + pt3.Y)/2, 0.0);
-                        else if (sVal.Equals("СПраво"))
-                            pt = new Point3d(pt3.X, (pt1.Y + pt3.Y)/2, 0.0);
+                        else if (sVal.Equals(Language.GetItem(LangItem, "kw6")))
+                            pt = new Point3d((pt1.X + pt3.X) / 2, pt1.Y, 0.0);
+                        else if (sVal.Equals(Language.GetItem(LangItem, "kw7")))
+                            pt = new Point3d((pt1.X + pt3.X) / 2, pt3.Y, 0.0);
+                        else if (sVal.Equals(Language.GetItem(LangItem, "kw8")))
+                            pt = new Point3d(pt1.X, (pt1.Y + pt3.Y) / 2, 0.0);
+                        else if (sVal.Equals(Language.GetItem(LangItem, "kw9")))
+                            pt = new Point3d(pt3.X, (pt1.Y + pt3.Y) / 2, 0.0);
 
                         var mat = Matrix3d.Scaling(doubleRes.Value, pt);
                         ent.TransformBy(mat);
@@ -119,7 +143,7 @@ namespace mpMultiScale
                 var tr = db.TransactionManager.StartTransaction();
                 using (tr)
                 {
-                    var btr = (BlockTableRecord) tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite, false);
+                    var btr = (BlockTableRecord)tr.GetObject(db.CurrentSpaceId, OpenMode.ForWrite, false);
                     foreach (var objId in idArray)
                     {
                         var ent = tr.GetObject(objId, OpenMode.ForWrite).Clone() as Entity;
